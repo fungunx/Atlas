506b8feb44f2241d5761afd05dbb0d571bd4d551
fungunx <fahrettinungun1@gmail.com>
2022-10-06 11:33:39 -0600

    Guncelleme

--- AtlasJAva.java
+++ AtlasJAva.java
@@ -2,5 +2,10 @@ public class AtlasJAva {
     public static void main(String[] args) {
         System.out.println("Atlas Ungun");
          System.out.println("Atlas Ungun2");
+         int a = 90;
+         int b=80;
+         if (a/b>=20){
+             System.out.println("Merhaba");}
+         else System.out.println("No");
     }
 }
