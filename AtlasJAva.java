c912dbfcf4f33c90635bc697cacfc80401ff8537
fungunx <fahrettinungun1@gmail.com>
2022-10-06 12:26:26 -0600

    New

--- AtlasJAva.java
+++ AtlasJAva.java
@@ -2,9 +2,9 @@ public class AtlasJAva {
     public static void main(String[] args) {
         System.out.println("Atlas Ungun");
          System.out.println("Atlas Ungun2");
-         int a = 90;
-         int b=80;
-         if (a/b>=20){
+         int a =80;
+         int b=2;
+         if (a/b>=40){
              System.out.println("Merhaba");}
          else System.out.println("No");
     }
