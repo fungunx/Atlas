705bbb1c3d9e191927bbdee8d205a9d09c33330d
fungunx <fahrettinungun1@gmail.com>
2022-10-06 12:55:15 -0600

    Galatasaray

--- Ungun.java
+++ Ungun.java
@@ -2,5 +2,6 @@ public class Ungun {
     public static void main(String[] args) {
         System.out.println("Hello Canada");
         System.out.println("Helle Ungun");
+        System.out.println("Helle Hello");
     }
 }
