ea311c1bc50bf38f2fdde9108f5c72018340b697
fungunx <fahrettinungun1@gmail.com>
2022-10-11 12:15:49 -0600

    merge operation

--- Ungun.java
+++ Ungun.java
@@ -1,11 +1,14 @@
+import static javax.swing.text.html.HTML.Tag.HEAD;
+
 public class Ungun {
     public static void main(String[] args) {
         System.out.println("Hello Canada");
-<<<<<<< HEAD
+
         System.out.println("Hello Ungun");
-=======
+
         System.out.println("Helle Ungun");
         System.out.println("Helle Hello");
->>>>>>> Canada
+
+
     }
 }
