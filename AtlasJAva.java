56cf2cf9920368a543d93e607866c9e0e56a1e3d
Fahrettin Ungun <fahrettinungun1@gmail.com>
2022-10-19 10:41:42 -0600

    asdAS

--- AtlasJAva.java
+++ AtlasJAva.java
@@ -2,13 +2,11 @@ public class AtlasJAva {
     public static void main(String[] args) {
         System.out.println("Atlas Ungun");
          System.out.println("Atlas Ungun2");
-<<<<<<< HEAD
-=======
          int a =80;
          int b=2;
          if (a/b>=40){
              System.out.println("Merhaba");}
          else System.out.println("No");
->>>>>>> 91a3491d787b039ccb20711c0aa346472dcece59
+
     }
 }
